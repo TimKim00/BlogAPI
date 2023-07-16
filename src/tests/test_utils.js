ddff53434a1f40cc88915d45ab2430dd93f35390
Yeonsu Kim <yeonsukim@yeonsus-mbp.mynetworksettings.com>
2023-07-16 10:42:19 -0700

    better comment testing util

--- src/tests/test_utils.js
+++ src/tests/test_utils.js
@@ -7,7 +7,7 @@ const Utils = {
         await this.clearPosts();
         await this.clearUser();
         return;
-    }, 
+    },
 
     async clearUser() {
         await pool.query('DELETE FROM users');
@@ -36,15 +36,27 @@ const Utils = {
             }
 
             for (let child of comment) {
-                if (!allCommentId.includes(child.commentId) || !traverseComments(child.replies)) {
-                    // If commentId is not in allCommentId or any of the child's replies is invalid, return false
+                const index = allCommentId.indexOf(child.commentId);
+                if (index !== -1) {
+                    allCommentId.splice(index, 1); // remove the matched commentId from allCommentId
+                    if (!traverseComments(child.replies)) {
+                        // If any of the child's replies is invalid, return false
+                        return false;
+                    }
+                } else {
+                    // If commentId is not in allCommentId, return false
                     return false;
                 }
             }
             return true;  // return true if all comments and replies are valid
         }
-    return traverseComments(allComments);
-}
+        const result = traverseComments(allComments);
+
+        // If there are any remaining ids in allCommentId, that means they weren't found in the comments
+        // so we return false. Otherwise, we return whatever result we got from traversing the comments.
+        return allCommentId.length === 0 && result;
+    }
+
 
 }
 
